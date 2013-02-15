69fd34e42960eaced65661d8038e27e1ed7bc8f8
Maria Anna Lind <maria.anna.lind@gmail.com>
2013-02-15 10:19:38 +0100

    first commit

---
+++ Assignment 2.2/src/WordPlay.java
@@ -0,0 +1,31 @@
+
+public class WordPlay {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String inputString = "Sex at noon taxes.".toLowerCase();
+		String textToCheck = inputString.replaceAll("\\W", "");
+        char[] textArray = textToCheck.toCharArray();
+        System.out.println(isPalindrom(textArray));
+        
+
+	}
+	
+	public static boolean isPalindrom(char[] inputText){
+	    int i1 = 0;
+	    int i2 = inputText.length - 1;
+	    while (i2 > i1) {
+	        if (inputText[i1] != inputText[i2]) {
+	            return false;
+	        }
+	        ++i1;
+	        --i2;
+	    }
+	    return true;
+	}
+
+}
+
+
